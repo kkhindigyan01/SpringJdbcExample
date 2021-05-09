f2a202fa3439145ac96b2ce79f134834cdc39002
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-05-09 21:08:15 +0530

    A text file is added and java doc is added in client program

--- src/main/java/com/kkhindigyan/org/client/ClientTest.java
+++ src/main/java/com/kkhindigyan/org/client/ClientTest.java
@@ -6,7 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.kkhindigyan.org.model.Employee;
 import com.kkhindigyan.org.service.EmployeeService;
 import com.kkhindigyan.org.service.EmployeeServiceImpl;
-
+/**
+ * @author Kishan Kumar
+ *This is client program
+ */
 public class ClientTest {
 
 	public static void main(String[] args) {
